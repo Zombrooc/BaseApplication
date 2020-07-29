b2dd34c9a7c06a22ff5756a9e015250273f1c2dc
Elian Valdez <elian.valdez09@gmail.com>
2020-07-29 17:45:53 -0300

    Lint Corretion

--- packages/server/src/config/upload.js
+++ packages/server/src/config/upload.js
@@ -1,26 +1,20 @@
 require('dotenv').config()
-const multer = require('multer');
-const path = require('path');
-
+const multer = require('multer')
+const path = require('path')
 
 module.exports = {
-  dest: path.resolve(__dirname, "..", "..", "uploads"),
+  dest: path.resolve(__dirname, '..', '..', 'uploads'),
   storage: multer.memoryStorage(),
   limits: {
     fileSize: 2 * 1024 * 1024
   },
   fileFilter: (req, file, cb) => {
-    const allowedMimes = [
-      "image/jpeg",
-      "image/pjpeg",
-      "image/png",
-      "image/gif"
-    ];
+    const allowedMimes = ['image/jpeg', 'image/pjpeg', 'image/png', 'image/gif']
 
     if (allowedMimes.includes(file.mimetype)) {
-      cb(null, true);
+      cb(null, true)
     } else {
-      cb(new Error("Invalid file type."));
+      cb(new Error('Invalid file type.'))
     }
   }
-};
+}
